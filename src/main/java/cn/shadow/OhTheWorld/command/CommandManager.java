e343a9cc5b2ed2d185cb310246516a98adbf5bbc
OhTheDungeon <wind_shadow@qq.com>
2021-02-07 00:14:39 -0800

    1.1

--- src/main/java/cn/shadow/OhTheWorld/command/CommandManager.java
+++ src/main/java/cn/shadow/OhTheWorld/command/CommandManager.java
@@ -2,12 +2,14 @@ package cn.shadow.OhTheWorld.command;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import cn.shadow.OhTheWorld.command.impl.CustomGeneratorText;
 import cn.shadow.OhTheWorld.command.impl.MainMenuCommand;
 import cn.shadow.OhTheWorld.command.impl.TeleportCommand;
 
 public class CommandManager {
 	public static void registerCommands(JavaPlugin plugin) {
 		plugin.getCommand("otw").setExecutor(new MainMenuCommand());
+		plugin.getCommand("otwg").setExecutor(new CustomGeneratorText());
 		plugin.getCommand("wtp").setExecutor(new TeleportCommand());
 	}
 }
